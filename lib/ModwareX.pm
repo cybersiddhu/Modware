1e1f45055c5c30b6f24bad5ef7219f1053eda2aa
Siddhartha Basu <biosidd@gmail.com>
2009-11-10 21:54:18 -0600

    initial commit

---
+++ lib/ModwareX.pm
@@ -0,0 +1,33 @@
+package ModwareX;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ModwareX -
+
+=head1 SYNOPSIS
+
+  use ModwareX;
+
+=head1 DESCRIPTION
+
+ModwareX is
+
+=head1 AUTHOR
+
+Siddhartha Basu E<lt>sidd.basu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
