4f86166cd42a36a80a9ac087c8dbcabd13e2b8d9
ubuntu <ubuntu@cgm_oracle.(none)>
2010-04-06 00:57:28 -0500

    fixed presence of pseudogene

--- bin/report/curation_report_standalone.pl
+++ bin/report/curation_report_standalone.pl
@@ -65,7 +65,7 @@ $writer->print(
 my $gene_rs = $schema->resultset('Sequence::Feature')->search(
     {   'type.name'     => 'gene',
         'me.is_deleted' => 0,
-        'me.name'       => { -not_like => '%RTE' }
+        'me.name'       => { -not_like => [ '%RTE', '%_ps' ] }
     },
     {   join     => [qw/type dbxref/],
         prefetch => [qw/dbxref/],
@@ -77,7 +77,7 @@ while ( my $gene = $gene_rs->next ) {
 
     my $trans_rs = $gene->feat_relationship_object_ids->search_related(
         'subject',
-        { 'type.name' => 'mRNA' },
+        { 'type.name' => [qw/mRNA pseudogene/] },
         { join        => 'type' }
     );
 
